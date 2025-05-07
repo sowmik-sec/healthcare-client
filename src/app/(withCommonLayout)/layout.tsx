cf5fc5a93b6ee3a904e41ed22088ea551424127e
sowmik-sec <sowmik.sec@gmail.com>
2025-05-07 12:22:49 +0600

    Customize Navbar and Create Footer. add necessary photos

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from "@/components/shared/Footer/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <>
       <Navbar />
-      {children}
-    </div>
+      <div className="min-h-screen">{children}</div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/shared/Footer/Footer.tsx
@@ -0,0 +1,58 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import facebookIcon from "@/assets/landing_page/facebook.png";
+
+const Footer = () => {
+  return (
+    <Box bgcolor="rgb(17, 26, 34)" py={5}>
+      <Container>
+        <Stack direction="row" justifyContent="center" gap={4}>
+          <Typography component={Link} href="/consultation" color="#fff">
+            Consultation
+          </Typography>
+          <Typography color="#fff">Health Plans</Typography>
+          <Typography color="#fff">Medicine</Typography>
+          <Typography color="#fff">Diagnostics</Typography>
+          <Typography color="#fff">NGOs</Typography>
+        </Stack>
+        <Stack direction="row" justifyContent="center" gap={2} py={2}>
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+        </Stack>
+        <div className="border-b-[1px] border-dashed"></div>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={2}
+          py={2}
+        >
+          <Typography component="p" color="#fff">
+            &copy; 2025 Ph HealthCare. All Rights Reserved.
+          </Typography>
+          <Typography
+            variant="h4"
+            component={Link}
+            href="/"
+            fontWeight={600}
+            color="white"
+          >
+            P
+            <Box component="span" color="primary.main">
+              H
+            </Box>{" "}
+            Health Care
+          </Typography>
+          <Typography component="p" color="#fff">
+            Privacy Policy Terms & conditions
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
