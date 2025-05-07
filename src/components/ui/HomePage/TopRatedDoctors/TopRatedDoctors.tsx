7701b2142e6b9cfe1d2981093ad47223c79ca330
sowmik-sec <sowmik.sec@gmail.com>
2025-05-07 17:57:40 +0600

    fix bug

--- src/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
+++ src/components/ui/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
@@ -4,15 +4,19 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardMedia,
   Container,
   Grid,
   Typography,
 } from "@mui/material";
 import Image from "next/image";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Link from "next/link";
 
 const TopRatedDoctors = async () => {
   const res = await fetch("http://localhost:5000/api/v1/doctor?page=1&limit=3");
   const { data: doctors } = await res.json();
+  //   console.log(doctors);
   return (
     <Box
       sx={{
@@ -33,11 +37,12 @@ const TopRatedDoctors = async () => {
           and top-quality surgery facilities right here.
         </Typography>
       </Box>
-      <Container>
+
+      <Container sx={{ margin: "30px auto" }}>
         <Grid container spacing={2}>
           {doctors.map((doctor: any) => (
-            <Grid item key={doctor.id} md={4}>
-              <Card>
+            <Grid container key={doctor.id} size={{ md: 4 }}>
+              <Card sx={{ marginX: "auto" }}>
                 <Box
                   sx={{
                     width: "100%",
@@ -65,7 +70,7 @@ const TopRatedDoctors = async () => {
                     {doctor.qualification}, {doctor.designation}
                   </Typography>
                   <Typography variant="body2" color="text.secondary" mt={1}>
-                    {/* <LocationOnIcon /> {doctor.address} */}
+                    <LocationOnIcon /> {doctor.address}
                   </Typography>
                 </CardContent>
                 <CardActions
@@ -82,6 +87,22 @@ const TopRatedDoctors = async () => {
             </Grid>
           ))}
         </Grid>
+        <Box
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              marginTop: "20px",
+            }}
+            component={Link}
+            href="/doctors"
+          >
+            View ALL
+          </Button>
+        </Box>
       </Container>
     </Box>
   );
