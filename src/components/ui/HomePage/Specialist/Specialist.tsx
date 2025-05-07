16ffddce0a9946823d5d6cb59443db4895b233a2
sowmik-sec <sowmik.sec@gmail.com>
2025-05-07 15:36:38 +0600

    Display Specialties Data and Apply Styles

--- src/components/ui/HomePage/Specialist/Specialist.tsx
+++ src/components/ui/HomePage/Specialist/Specialist.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 const Specialist = async () => {
@@ -7,8 +8,8 @@ const Specialist = async () => {
       revalidate: 30,
     },
   });
-  const specialists = await res.json();
-  console.log(specialists);
+  const { data: specialists } = await res.json();
+
   return (
     <Container>
       <Box sx={{ margin: "40px 0px", textAlign: "center" }}>
@@ -19,6 +20,42 @@ const Specialist = async () => {
           <Typography component="p" fontWeight={300} fontSize={18}>
             Experienced Doctors Across All Specialists
           </Typography>
+        </Box>
+        <Stack direction="row" gap={4} mt={5}>
+          {specialists.map((specialty: any) => (
+            <Box
+              key={specialty.id}
+              sx={{
+                flex: 1,
+                width: "150px",
+                backgroundColor: "rgba(245,245,245,1)",
+                border: "1px solid rgba(250, 250, 250,1)",
+                borderRadius: "10px",
+                textAlign: "center",
+                padding: "40px 10px",
+                "& img": {
+                  width: "50px",
+                  height: "50px",
+                  margin: "0 auto",
+                },
+                "&:hover": {
+                  border: "1px solid blue",
+                  padding: "40px 10px",
+                  borderRadius: "10px",
+                },
+              }}
+            >
+              <Image src={specialty.icon} alt="" width={100} height={100} />
+              <Box>
+                <Typography component="p" fontWeight={600} fontSize={18} mt={2}>
+                  {specialty.title}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Box sx={{ marginTop: "20px" }}>
+          <Button variant="outlined">View All</Button>
         </Box>
       </Box>
     </Container>
