af72942148a341f3113bce9fe89e1f48eaf85013
sowmik-sec <sowmik.sec@gmail.com>
2025-05-07 15:05:50 +0600

    Creating The Hero Section Segment-2

--- src/components/ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/ui/HomePage/HeroSection/HeroSection.tsx
@@ -34,18 +34,74 @@ const HeroSection = () => {
           variant="h6"
           component="p"
           fontWeight={400}
-          sx={{ width: "50%", marginY: "20px" }}
+          sx={{ marginY: "20px" }}
         >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
           voluptatem, quis quas quibusdam quos temporibus consequuntur eos unde,
           totam error ullam dolore.
         </Typography>
-        <Button>Make Appointment</Button>
-        <Button variant="outlined" sx={{ marginLeft: "5px" }}>
-          Contact Us
-        </Button>
+        <Box sx={{ display: "flex", width: "50%" }}>
+          <Button>Make Appointment</Button>
+          <Button variant="outlined" sx={{ marginLeft: "5px" }}>
+            Contact Us
+          </Button>
+        </Box>
       </Box>
-      <Box></Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        <Box sx={{ position: "absolute", left: "200px", top: "-30px" }}>
+          <Image src={assets.svgs.arrow} width={100} height={100} alt="arrow" />
+        </Box>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              alt="doctor 1"
+              width={240}
+              height={380}
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              alt="doctor 1"
+              width={240}
+              height={350}
+            />
+          </Box>
+        </Box>
+        <Box sx={{ position: "absolute", top: "220px", left: "150px" }}>
+          <Image
+            src={assets.images.doctor3}
+            alt="doctor 3"
+            width={240}
+            height={240}
+          />
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: "0px",
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            alt="doctor 3"
+            width={180}
+            height={180}
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
