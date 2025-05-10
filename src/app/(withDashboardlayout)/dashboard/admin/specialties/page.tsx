455aac953d93b2b44a506ee0d0f172a69b5b395b
sowmik-sec <sowmik.sec@gmail.com>
2025-05-10 14:30:44 +0600

    Start Developing Specialist Feature in Admin Dashboard

--- src/app/(withDashboardlayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardlayout)/dashboard/admin/specialties/page.tsx
@@ -1,7 +1,17 @@
+import PHModal from "@/components/shared/PHModal/PHModal";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import React from "react";
 
 const SpecialtiesPage = () => {
-  return <div>specialties</div>;
+  return (
+    <Box>
+      <Stack direction="row">
+        <Button>Create Specialty</Button>
+        <PHModal />
+        <TextField size="small" placeholder="Search Specialty" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
