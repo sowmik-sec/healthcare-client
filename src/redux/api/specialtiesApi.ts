3a83a4d1c4d7ea348f813d251ddf62bbe9b16b60
sowmik-sec <sowmik.sec@gmail.com>
2025-05-10 14:54:29 +0600

    Using Form Component Inside Reusable Modal

---
+++ src/redux/api/specialtiesApi.ts
@@ -0,0 +1,19 @@
+import { tagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const specialtiesApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createSpecialty: build.mutation({
+      query: (data) => ({
+        url: "/specialties",
+
+        method: "POST",
+        contentType: "multipart/form-data",
+        data,
+      }),
+      invalidatesTags: [tagTypes.specialties],
+    }),
+  }),
+});
+
+export const { useCreateSpecialtyMutation } = specialtiesApi;
