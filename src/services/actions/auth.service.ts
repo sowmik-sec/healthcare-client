4927f1c72c2bfdfdc0de029f0f994365728a8597
sowmik-sec <sowmik.sec@gmail.com>
2025-05-11 21:08:00 +0600

    fix auth service

--- src/services/actions/auth.service.ts
+++ src/services/actions/auth.service.ts
@@ -1,24 +1,29 @@
 import { authKey } from "@/constants/authKey";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
 import { decodedToken } from "@/utils/jwt";
+
 import {
   getFromLocalStorage,
   removeFromLocalStorage,
   setToLocalStorage,
 } from "@/utils/local-storage";
-import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+  //   console.log(accessToken);
   return setToLocalStorage(authKey, accessToken);
 };
 
 export const getUserInfo = () => {
   const authToken = getFromLocalStorage(authKey);
-  if (typeof authToken === "string" && authToken !== "undefined") {
+  //   console.log(authToken);
+  if (authToken) {
     const decodedData: any = decodedToken(authToken);
     return {
       ...decodedData,
-      role: decodedData?.role.toLowerCase(),
+      role: decodedData?.role?.toLowerCase(),
     };
+  } else {
+    return "";
   }
 };
 
