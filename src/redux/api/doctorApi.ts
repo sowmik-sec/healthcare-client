d63520d6bbd0ece6d855e8fa3ead01bde4924b97
sowmik-sec <sowmik.sec@gmail.com>
2025-05-11 11:59:58 +0600

    Detailing the Functionality of the Reusable Doctor Modal

---
+++ src/redux/api/doctorApi.ts
@@ -0,0 +1,45 @@
+import { IDoctor } from "@/types/doctor";
+import { tagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+import { IMeta } from "@/types";
+
+export const doctorApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createDoctor: build.mutation({
+      query: (data) => ({
+        url: "/user/create-doctor",
+        method: "POST",
+        contentType: "multipart/form-data",
+        data,
+      }),
+      invalidatesTags: [tagTypes.doctor],
+    }),
+    getAllDoctors: build.query({
+      query: (args: Record<string, any>) => ({
+        url: "/doctor",
+        method: "GET",
+        params: args,
+      }),
+      transformResponse: (response: IDoctor[], meta: IMeta) => {
+        return {
+          doctors: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.doctor],
+    }),
+    deleteDoctor: build.mutation({
+      query: (id) => ({
+        url: `/doctor/soft/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.doctor],
+    }),
+  }),
+});
+
+export const {
+  useCreateDoctorMutation,
+  useGetAllDoctorsQuery,
+  useDeleteDoctorMutation,
+} = doctorApi;
