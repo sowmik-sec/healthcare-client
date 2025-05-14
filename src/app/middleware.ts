169e9dadb46781d64de1e624081dcaa4c58d30c1
sowmik-sec <sowmik.sec@gmail.com>
2025-05-14 11:20:34 +0600

    Route Protection In NextJs with Nextjs Middleware Part-2

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,24 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const commonPrivateRoutes = [
+  "/dashboard",
+  "/dashboard/change-password",
+  "/doctors",
+];
+
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const accessToken = cookies().get("accessToken")?.value;
+  if (!accessToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (accessToken && commonPrivateRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
   return NextResponse.redirect(new URL("/home", request.url));
 }
 
